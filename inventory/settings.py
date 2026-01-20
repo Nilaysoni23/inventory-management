02b8255277f3499bd637b47573cae05848bed4cd
nilay_fichadia <nilayfichadia@gmail.com>
2026-01-20 21:08:20 +0530

    Create admin user via migration

--- inventory/settings.py
+++ inventory/settings.py
@@ -149,13 +149,3 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-if os.environ.get("RENDER") == "true":
-    from django.contrib.auth import get_user_model
-    User = get_user_model()
-
-    if not User.objects.filter(username="admin").exists():
-        User.objects.create_superuser(
-            username="admin",
-            email="admin@example.com",
-            password="admin123"
-        )
